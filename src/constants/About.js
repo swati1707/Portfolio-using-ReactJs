f6a6c72152144c355d5dd1fd9d4de07b8d16ab20
Swati_Wuppuluri <wswati.sdw@gmail.com>
2020-09-06 16:19:40 +0530

    Initial commt

---
+++ src/constants/About.js
@@ -0,0 +1,5 @@
+export const about = {
+    "heading": "About Me",
+    // "quote": "If you love life, don't waste time, \nfor time is what life is made up of",
+    "bio": "Front-end developer, passionate, MERN stack developer, a tech enthusiast, tech blogger. \n\nTechnology makes the world better and I am interested to learn new technologies using which I can make life easier."
+};
