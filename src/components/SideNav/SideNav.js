d0d63d76796d3ce36acaf86ca1f374e9c4574997
Swati_Wuppuluri <wswati.sdw@gmail.com>
2021-01-01 17:12:01 +0530

    data changes

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,18 +1,17 @@
 import React from 'react';
-
 import './SideNav.css';
+import logo from './photo.jpg';
 
 export const SideNav = ({ handleSectionClick }) => {
     return (
         <div className="sidenav">
             <img
-                src="https://github.com/swati1707/Portfolio-using-ReactJs/blob/master/src/components/SideNav/avatar-img1.jpg"
+                src={logo}
                 className="avatar"
                 alt="avatar"
                 
             />
             <h2 className="main-name">Swati Wuppuluri</h2>
-            {/* <p className="subtitle">"A Cadet Who Codes"</p> */}
             <div className="sections-list">
                 <p className="section-list-element" onClick={() => handleSectionClick("about")}>About Me</p>
                 <p className="section-list-element" onClick={() => handleSectionClick("skills")}>Skills</p>
--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -3,17 +3,44 @@ export const projects = {
     quote: "Ahh, Learning alone is boring, implementation is the real fun",
     projectList: [
         {
-            name: "Portfolio using ReactJS",
+            name: "Portfolio",
             description: "To build your own portfolio using ReactJs by making different components which makes development easier and less complex.",
             link: "https://github.com/swati1707/Portfolio-using-ReactJs"
-        }, {
+        },
+        {
             name: "Weather app using RaectJS",
             description: "Weather application using ReactJS which displays weather conditions of different cities.",
             link: "https://github.com/swati1707/weather-app"
-        }, {
+        },
+        {
             name: "Weather app using Node.js",
             description: "Weather application using Node.js which displays weather conditions of different cities.",
             link: "https://github.com/swati1707/node-weatherapp"
+        },
+        {
+            name: "Shopping cart",
+            description: "Shopping cart built using React.js & Redux which displays products to buy & user can add the products to cart",
+            link: "https://github.com/swati1707/Shopping-Cart-using-React-and-Redux"
+        },
+        {
+            name: "MERN stack CRUD operations",
+            description: "CRUD operations implemented using MERN stack",
+            link: "https://github.com/swati1707/MERN-stack-CRUD-operations"
+        },
+        {
+            name: "React Firebase Authentication",
+            description: "Authentication with different social media accounts implemented using React.js & Firebase",
+            link: "https://github.com/swati1707/React-firebase-authentication"
+        },
+        {
+            name: "Authentication using JWT",
+            description: "Authentication implemented using JWT in MERN",
+            link: "https://github.com/swati1707/Authentication-using-JWT-in-MERN"
+        },
+        {
+            name: "Chat application",
+            description: "Chat Application implemented using React.js & Node.js",
+            link: "https://github.com/swati1707/chat-applicaion-using-reactjs-and-nodejs"
         }
     ]
 }
